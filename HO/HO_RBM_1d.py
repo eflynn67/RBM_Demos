fe24c4ea2b9be39ae80cf0c1d438158bfa514d19
Eric Flynn <flynner6@msu.edu>
2022-03-16 22:20:30 -0400

    updating HO script

--- HO/HO_RBM_1d.py
+++ HO/HO_RBM_1d.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 import multiprocessing as mp
 import pathos
 ### NOTE: hbar = 1 in this demo
+### First define exact solutions to compare numerical solutions to.
 def getPsi_x(n,nu):
     '''
-    Definition of HO wavefunction taken from Zettili page 240.
-    
+    Definition of exact HO wavefunction taken from Zettili page 240.
     
     Parameters
     ----------
@@ -31,38 +31,62 @@ def getPsi_x(n,nu):
         return(result)
     return wf
 def getExactE(n,omega):
+    '''
+    Exact energies of the 1d harmonic oscillator. 
+
+    Parameters
+    ----------
+    n : float
+        principle quantum number. float integers
+    omega : float
+        oscillator frequency.
+
+    Returns
+    -------
+    float
+        oscillator energy. 
+
+    '''
     return (n+ .5)*omega
 ###  HO parameters
 mu = 1.0
 omega = 1.0
-n_val = 0 ## n-th wavefunction
-nu = np.sqrt(1/mu*omega)
-
+n_max = 3 # maximum number of HO states to consider
+# define oscillator length scale x0 
+x0 = np.sqrt(1/mu*omega)
+# define n array
+n_vals = np.arange(0,n_max,1)
+x0_vals = np.linspace(x0, 5*x0,5)
 
 ### grid spacing
 h = 10**(-3)
 
 # define the domain
-x_a = -10 # left boundary 
-x_b = 10 # right boundary 
+x_a = -5 # left boundary 
+x_b = 5 # right boundary 
 
 x_array = np.arange(x_a,x_b+h,h)
 m = len(x_array) 
+print(m)
+## Now numericaly solve the 1-d SE. We do this in 2 ways. First we construct 
+## the discretized representation of the Hamiltonian H and use LA to diagonalize it
+## Second, we use an iterative scheme to solve the ODE without explictly constructing
+## the matrix H. 
+def V(x,x0):
+    return .5*(x**2)/x0**4
 
+off_diag = np.zeros(m)
+off_diag[1] = 1
 
-#### Now we construct a reduced basis.
-n_vals = np.arange(0,5,1)
-nu_vals = np.linspace(nu, 5*nu,5)
+H = -1*(-2*np.identity(m) + sci.linalg.toeplitz(off_diag))/(2*mu*h**2) + np.diag(V(x_array,x0))
 
-
-T = np.zeros((len(n_vals),m))
+evals,evects = np.linalg.eigh(H)
+norm = np.sqrt(1/sci.integrate.simpson(evects[:,0]*evects[:,0],x_array))
+print(norm)
+plt.plot(x_array,evects[:,0]*norm)
+plt.show()
 '''
-#plt.plot(x_array,T[40])
-#plt.show()
-#plt.plot(x_array,V[0],'--',label='evector')
-#plt.plot(x_array,T[0],'-',label='exact')
-#plt.legend()
-#plt.show()
+
 #### Now numericaly solve the 1-d SE. We use 4th order Runge-Kutta here.
 #### define 'inhomogenous' part of system of first order odes
 def f(r,x,mass,energy,nu):
@@ -98,14 +122,14 @@ init_coords = np.array([0.0,1.0])
 E1 = 0.0
 E2 = .6
 
-wf_sol_init,boundary2 = solve(E1,init_coords,mu,nu,x_array)
+wf_sol_init,boundary2 = solve(E1,init_coords,mu,x0,x_array)
 wf_exact = psi(x_array)
 
 
 eps = 10**(-5)
 while abs(E2 - E1) > eps:
     print(abs(E2-E1))
-    boundary1, (wf_sol, boundary2) = boundary2, solve(E2,init_coords,mu,nu,x_array)
+    boundary1, (wf_sol, boundary2) = boundary2, solve(E2,init_coords,mu,x0,x_array)
     E1,E2 = E2, E2 - boundary2*(E2-E1)/(boundary2 - boundary1 )
 norm = np.sqrt(integrate.simpson(wf_sol[:,0]**2,x_array))
 wf_sol = wf_sol/norm
@@ -118,5 +142,4 @@ plt.plot(x_array,wf_sol[:,0],label='RK sol')
 plt.plot(x_array,wf_exact,label='exact')
 plt.legend()
 plt.show()
-
 '''
